2ba26b914f4495fc452baac1e1f8bb4dadd78d3c
fadli <fadlirahmanfauzan04@gmail.com>
2025-03-04 08:33:55 +0700

    add cors handling

--- src/App.ts
+++ src/App.ts
@@ -4,10 +4,19 @@ import EmployeeRoute from "./route/EmployeeRoute";
 import ProductRoute from "./route/ProductRoute";
 import CustomerRoute from "./route/CustomerRoute";
 import TransactionRoute from "./route/TransactionRoute";
-
+import cors from "cors";
+import { config } from "./config/env";
 
 const app = express();
 app.use(express.json());
+
+
+// Setup CORS pakai env
+app.use(cors({
+    origin: config.CORS_ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use("/auth", AuthRoute);
 app.use("/employees", EmployeeRoute);
--- src/config/env.ts
+++ src/config/env.ts
@@ -5,4 +5,5 @@ dotenv.config();
 export const config = {
   PORT: process.env.PORT || 3000,
   JWT_SECRET: process.env.JWT_SECRET || "secretkey123",
+  CORS_ORIGIN: process.env.CORS_ORIGIN || "http://localhost:5173"
 };
